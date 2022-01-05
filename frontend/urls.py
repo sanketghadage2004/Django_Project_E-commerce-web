fb271ab8ee14f6c1a8ee32c18087fb1eeccd94c3
sanket ghadage <sanketghadage2004@gmail.com>
2022-01-05 18:21:49 +0530

    showing product images on webpages by using static path ,and adding url to home page and product listing page
    showing product images on webpages by using static path ,and adding url to home page and product listing page

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from frontend import views
 urlpatterns = [
     path('',views.home_page, name='home_page'),
+    path('product-listing-page/', views.ProductListingView.as_view(), name="ProductListingView")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, request
+from django.views import View
 # Create your views here.
 
 def home_page(request):
     return render(request, 'home.html')
+
+
+class ProductListingView(View):
+
+    def get(self, request):
+        return render(request, 'product_listing.html') 
