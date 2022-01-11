02588df8468ae094553c96417cb0a2b1e65c91a9
sanket ghadage <sanketghadage2004@gmail.com>
2022-01-11 14:55:06 +0530

    Cart
    cart functionality
    add to cart and total and sub total

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-to-cart', views.addToCart, name='addToCart')
+    path('add-to-cart', views.addToCart, name='addToCart'),
+    path('my-cart', views.MyCart.as_view(), name='MyCart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+from django.views.generic.base import View
+
+import cart
 from . models import Cart
+from django.views import View
+from product.models import ProductCategory, Product
+
 # Create your views here.
 
 
@@ -8,19 +14,49 @@ def addToCart(request):
     product_id = request.POST.get('product_id')
     quantity = int(request.POST.get('quantity'))
 
-    try:
-        checkCart = Cart.objects.get(user=request.user, Product_id=product_id )
-        checkCart.quantity = checkCart.quantity +quantity
-        checkCart.save()
-    
-    except: Cart.DoesNotExist
-
-    Cart.objects.create(
-        user = request.user,
-        Product_id= product_id,
-        quantity=quantity,
-
-    )
+    cart, flag = Cart.objects.get_or_create(user=request.user, Product_id=product_id )
+    if flag:
+        cart.quantity = quantity
+    else:    
+        cart.quantity = quantity + cart.quantity
+    cart.save()
 
     return redirect('ProductDetailsView',product_id=product_id)
+
+class MyCart(View):
     
+    template_name = 'my-cart.html'
+
+    def get(self,request):
+        product_categories = ProductCategory.objects.filter(status=True)
+        cartProducts = Cart.objects.filter(user=request.user)
+
+        carts = {}
+        subTotal = 0
+        total = 0
+        shippingCost = 50
+        for key, cartProduct in enumerate(cartProducts):
+            productTotal = int(cartProduct.quantity) * int(cartProduct.Product.price)
+            total += productTotal
+            subTotal += productTotal
+            carts[key] = {
+                'product_image': cartProduct.Product.cover_image,
+                'product_name': cartProduct.Product.name,
+                'product_price': cartProduct.Product.price,
+                'quantity': cartProduct.quantity,
+                'productTotal': productTotal
+            }
+        
+        
+        total = shippingCost + subTotal
+        carts = list(carts.values())
+
+        context = {
+            'productCategories': product_categories,
+            'cartProducts': carts,
+            'subTotal':subTotal,
+            'shippingCost': shippingCost,
+            'total':total,
+        }
+
+        return render(request, self.template_name, context)
