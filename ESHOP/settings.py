ca2ea6743ee074562771b34dd7e1981bedfa33a1
sanket ghadage <sanketghadage8008@gmail.com>
2022-04-15 11:01:29 +0530

    heroku

--- ESHOP/settings.py
+++ ESHOP/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3i-8*$x!055$^k(koy57nldk)z=gh+xfqtm3vn18h0a84b8x54
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["e-shop-by-sanket.herokuapp.com","127.0.0.1:8000"]
+ALLOWED_HOSTS = ["e-shop-by-sanket.herokuapp.com","http://127.0.0.1:8000/"]
 # 127.0.0.1
 # "e-shop-by-sanket.herokuapp.com","localhost"]
 
