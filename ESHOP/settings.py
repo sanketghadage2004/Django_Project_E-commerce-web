5a835bba4f2f3f59e9736c263dc7ddc6d7f81f14
sanket ghadage <sanketghadage8008@gmail.com>
2022-04-15 19:13:45 +0530

    heroku

--- ESHOP/settings.py
+++ ESHOP/settings.py
@@ -28,12 +28,10 @@ SECRET_KEY = 'django-insecure-3i-8*$x!055$^k(koy57nldk)z=gh+xfqtm3vn18h0a84b8x54
 # os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 # config('DEBUG', default=False, cast = bool)
 
 ALLOWED_HOSTS = ["e-shop-by-sanket.herokuapp.com","127.0.0.1","localhost",]
-# 127.0.0.1
-# "e-shop-by-sanket.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -164,5 +162,4 @@ REST_FRAMEWORK = {
 # Simplified static file serving.
 STATIC_FILE = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
